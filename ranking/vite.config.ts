2077cb390abab8d30e1154043fbe17ed3c6d8623
Hemenson Campos <hemensoncampos@gmail.com>
2026-02-16 03:51:55 -0300

    feat: add legacy browser support for smart tvs (lg 2020)

--- ranking/vite.config.ts
+++ ranking/vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+import legacy from '@vitejs/plugin-legacy'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    legacy({
+      targets: ['chrome 68', 'defaults', 'not IE 11'],
+    }),
+  ],
+  build: {
+    target: 'es2015',
+  }
 })
